4be643241a0a7dff4007d0a2588bbe9d90ebe427
silent-tempest <brussian768@gmail.com>
2018-10-09 20:18:06 +0700

    Changed settings.

--- core/settings.js
+++ core/settings.js
@@ -1,9 +1,6 @@
 'use strict';
 
-module.exports = {
-
-  /**
-   * Do use degrees instead of radians in the Vectors.
-   */
-  degress: false
-};
+/**
+ * Использовать градусы вместо радианов.
+ */
+exports.degress = false;

acf77c29752a0bac7d6a0d387c31f17ef14f5eb5
silent-tempest <brussian768@gmail.com>
2018-10-13 01:58:59 +0700

    Created test file for "v6.mat3".

---
+++ test/core/math/mat3.test.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var mat3 = require( '../../../core/math/mat3' );
+
+describe( 'v6.mat3', function ()
+{
+  it( 'successfully required', function ()
+  {
+    mat3.should.be.an( 'object' );
+  } );
+} );

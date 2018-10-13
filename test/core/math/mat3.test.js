d3407d52af717e3c09b0b92c6dca67a86e06123c
silent-tempest <brussian768@gmail.com>
2018-10-13 11:53:28 +0700

    Wrote some tests for "v6.mat3".

--- test/core/math/mat3.test.js
+++ test/core/math/mat3.test.js
@@ -4,8 +4,65 @@ var mat3 = require( '../../../core/math/mat3' );
 
 describe( 'v6.mat3', function ()
 {
+  var identity, m1, m2;
+
   it( 'successfully required', function ()
   {
     mat3.should.be.an( 'object' );
+  } );
+
+  beforeEach( function ()
+  {
+    identity = [
+      1, 0, 0,
+      0, 1, 0,
+      0, 0, 1
+    ];
+
+    m1 = [
+      3, 2, 1,
+      6, 5, 4,
+      9, 8, 7
+    ];
+
+    m2 = [
+      3, 6, 9,
+      2, 5, 8,
+      1, 4, 7
+    ];
+  } );
+
+  describe( 'v6.mat3.identity', function ()
+  {
+    it( 'works', function ()
+    {
+      mat3.identity().should.have.ordered.members( identity );
+    } );
+  } );
+
+  describe( 'v6.mat3.setIdentity', function ()
+  {
+    it( 'works', function ()
+    {
+      mat3.setIdentity( m1 );
+      m1.should.have.ordered.members( identity );
+    } );
+  } );
+
+  describe( 'v6.mat3.copy', function ()
+  {
+    it( 'works', function ()
+    {
+      mat3.copy( m1, m2 );
+      m1.should.have.ordered.members( m2 );
+    } );
+  } );
+
+  describe( 'v6.mat3.clone', function ()
+  {
+    it( 'works', function ()
+    {
+      mat3.clone( m1 ).should.have.ordered.members( m1 ).but.not.equal( m1 );
+    } );
   } );
 } );

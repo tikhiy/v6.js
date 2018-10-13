6f66897c8d9b83db2094c6a0c9bc28eaf187234c
silent-tempest <brussian768@gmail.com>
2018-10-13 12:52:59 +0700

    Improved "v6.Vector2D" code coverage.

--- test/core/math/Vector2D.test.js
+++ test/core/math/Vector2D.test.js
@@ -6,51 +6,57 @@ describe( 'v6.Vector2D', function ()
 {
   it( 'successfully required', function ()
   {
-    Vector2D
-      .should
-        .a( 'function' );
+    Vector2D.should.be.a( 'function' );
   } );
 
   describe( 'new v6.Vector2D', function ()
   {
-    it( 'works', function ()
+    it( 'works #1', function ()
     {
-      new Vector2D()
-        .should
-          .like( { x: 0, y: 0 } );
+      new Vector2D().should.be.like( { x: 0, y: 0 } );
+    } );
 
-      new Vector2D( 4, 2 )
-        .should
-          .like( { x: 4, y: 2 } );
+    it( 'works #2', function ()
+    {
+      new Vector2D( 4, 2 ).should.be.like( { x: 4, y: 2 } );
     } );
 
     describe( 'new v6.Vector2D.set', function ()
     {
-      it( 'works', function ()
+      it( 'works #1', function ()
       {
-        new Vector2D().set( 4, 2 )
-          .should
-            .like( { x: 4, y: 2 } );
+        new Vector2D().set( 4, 2 ).should.be.like( { x: 4, y: 2 } );
+      } );
+
+      it( 'works #2', function ()
+      {
+        new Vector2D( 4, 2 ).set().should.be.like( { x: 0, y: 0 } );
       } );
     } );
 
     describe( 'new v6.Vector2D.add', function ()
     {
-      it( 'works', function ()
+      it( 'works #1', function ()
       {
-        new Vector2D().add( 4, 2 )
-          .should
-            .like( { x: 4, y: 2 } );
+        new Vector2D().add( 4, 2 ).should.be.like( { x: 4, y: 2 } );
+      } );
+
+      it( 'works #2', function ()
+      {
+        new Vector2D( 4, 2 ).add().should.be.like( { x: 4, y: 2 } );
       } );
     } );
 
     describe( 'new v6.Vector2D.sub', function ()
     {
-      it( 'works', function ()
+      it( 'works #1', function ()
       {
-        new Vector2D().sub( 4, 2 )
-          .should
-            .like( { x: -4, y: -2 } );
+        new Vector2D().sub( 4, 2 ).should.be.like( { x: -4, y: -2 } );
+      } );
+
+      it( 'works #2', function ()
+      {
+        new Vector2D( 4, 2 ).sub().should.be.like( { x: 4, y: 2 } );
       } );
     } );
 
@@ -58,9 +64,7 @@ describe( 'v6.Vector2D', function ()
     {
       it( 'works', function ()
       {
-        new Vector2D( 4, 2 ).mul( 2 )
-          .should
-            .like( { x: 8, y: 4 } );
+        new Vector2D( 4, 2 ).mul( 2 ).should.be.like( { x: 8, y: 4 } );
       } );
     } );
 
@@ -68,19 +72,20 @@ describe( 'v6.Vector2D', function ()
     {
       it( 'works', function ()
       {
-        new Vector2D( 4, 2 ).div( 2 )
-          .should
-            .like( { x: 2, y: 1 } );
+        new Vector2D( 4, 2 ).div( 2 ).should.be.like( { x: 2, y: 1 } );
       } );
     } );
 
     describe( 'new v6.Vector2D.dot', function ()
     {
-      it( 'works', function ()
+      it( 'works #1', function ()
       {
-        new Vector2D( 4, 2 ).dot( 2, 3 )
-          .should
-            .equal( 8 + 6 );
+        new Vector2D( 4, 2 ).dot( 2, 3 ).should.equal( 8 + 6 );
+      } );
+
+      it( 'works #2', function ()
+      {
+        new Vector2D( 4, 2 ).dot().should.equal( 0 );
       } );
     } );
 
@@ -88,79 +93,123 @@ describe( 'v6.Vector2D', function ()
     {
       it( 'works', function ()
       {
-        new Vector2D( 4, 2 ).lerp( 8, 4, 0.5 )
-          .should
-            .like( { x: 6, y: 3 } );
+        new Vector2D( 4, 2 ).lerp( 8, 4, 0.5 ).should.be.like( { x: 6, y: 3 } );
       } );
     } );
 
     describe( 'new v6.Vector2D.setVector', function ()
     {
-      it( 'works', function ()
+      it( 'works #1', function ()
       {
-        new Vector2D().setVector( new Vector2D( 4, 2 ) )
-          .should
-            .like( { x: 4, y: 2 } );
+        new Vector2D( 1, 2 ).setVector( new Vector2D( 4, 2 ) ).should.be.like( { x: 4, y: 2 } );
+      } );
+
+      it( 'works #2', function ()
+      {
+        new Vector2D( 1, 2 ).setVector( {} ).should.be.like( { x: 0, y: 0 } );
+      } );
+
+      it( 'works #3', function ()
+      {
+        new Vector2D( 1, 2 ).setVector( { x: 4, y: 2 } ).should.be.like( { x: 4, y: 2 } );
       } );
     } );
 
     describe( 'new v6.Vector2D.addVector', function ()
     {
-      it( 'works', function ()
+      it( 'works #1', function ()
       {
-        new Vector2D().addVector( new Vector2D( 4, 2 ) )
-          .should
-            .like( { x: 4, y: 2 } );
+        new Vector2D( 1, 2 ).addVector( new Vector2D( 4, 2 ) ).should.be.like( { x: 5, y: 4 } );
+      } );
+
+      it( 'works #2', function ()
+      {
+        new Vector2D( 1, 2 ).addVector( {} ).should.be.like( { x: 1, y: 2 } );
+      } );
+
+      it( 'works #3', function ()
+      {
+        new Vector2D( 1, 2 ).addVector( { x: 4, y: 2 } ).should.be.like( { x: 5, y: 4 } );
       } );
     } );
 
     describe( 'new v6.Vector2D.subVector', function ()
     {
-      it( 'works', function ()
+      it( 'works #1', function ()
       {
-        new Vector2D().subVector( new Vector2D( 4, 2 ) )
-          .should
-            .like( { x: -4, y: -2 } );
+        new Vector2D( 1, 2 ).subVector( new Vector2D( 4, 2 ) ).should.be.like( { x: -3, y: 0 } );
+      } );
+
+      it( 'works #2', function ()
+      {
+        new Vector2D( 1, 2 ).subVector( {} ).should.be.like( { x: 1, y: 2 } );
+      } );
+
+      it( 'works #3', function ()
+      {
+        new Vector2D( 1, 2 ).subVector( { x: 4, y: 2 } ).should.be.like( { x: -3, y: 0 } );
       } );
     } );
 
     describe( 'new v6.Vector2D.mulVector', function ()
     {
-      it( 'works', function ()
+      it( 'works #1', function ()
       {
-        new Vector2D( 4, 2 ).mulVector( new Vector2D( 2, 3 ) )
-          .should
-            .like( { x: 8, y: 6 } );
+        new Vector2D( 4, 2 ).mulVector( new Vector2D( 2, 3 ) ).should.be.like( { x: 8, y: 6 } );
+      } );
+
+      it( 'works #2', function ()
+      {
+        new Vector2D( 4, 2 ).mulVector( { x: 2, y: 3 } ).should.be.like( { x: 8, y: 6 } );
       } );
     } );
 
     describe( 'new v6.Vector2D.divVector', function ()
     {
-      it( 'works', function ()
+      it( 'works #1', function ()
       {
-        new Vector2D( 4, 2 ).divVector( new Vector2D( 2, 3 ) )
-          .should
-            .like( { x: 4 / 2, y: 2 / 3 } );
+        new Vector2D( 4, 2 ).divVector( new Vector2D( 2, 3 ) ).should.be.like( { x: 4 / 2, y: 2 / 3 } );
+      } );
+
+      it( 'works #2', function ()
+      {
+        new Vector2D( 4, 2 ).divVector( { x: 2, y: 3 } ).should.be.like( { x: 4 / 2, y: 2 / 3 } );
       } );
     } );
 
     describe( 'new v6.Vector2D.dotVector', function ()
     {
-      it( 'works', function ()
+      it( 'works #1', function ()
       {
-        new Vector2D( 4, 2 ).dotVector( new Vector2D( 2, 3 ) )
-          .should
-            .equal( 8 + 6 );
+        new Vector2D( 4, 2 ).dotVector( new Vector2D( 2, 3 ) ).should.equal( 8 + 6 );
+      } );
+
+      it( 'works #2', function ()
+      {
+        new Vector2D( 4, 2 ).dotVector( {} ).should.equal( 0 );
+      } );
+
+      it( 'works #3', function ()
+      {
+        new Vector2D( 4, 2 ).dotVector( { x: 2, y: 3 } ).should.equal( 8 + 6 );
       } );
     } );
 
     describe( 'new v6.Vector2D.lerpVector', function ()
     {
-      it( 'works', function ()
+      it( 'works #1', function ()
       {
-        new Vector2D( 4, 2 ).lerpVector( new Vector2D( 8, 4 ), 0.5 )
-          .should
-            .like( { x: 6, y: 3 } );
+        new Vector2D( 4, 2 ).lerpVector( new Vector2D( 8, 4 ), 0.5 ).should.be.like( { x: 6, y: 3 } );
+      } );
+
+      it( 'works #2', function ()
+      {
+        new Vector2D( 4, 2 ).lerpVector( {}, 0.5 ).should.be.like( { x: 4, y: 2 } );
+      } );
+
+      it( 'works #3', function ()
+      {
+        new Vector2D( 4, 2 ).lerpVector( { x: 8, y: 4 }, 0.5 ).should.be.like( { x: 6, y: 3 } );
       } );
     } );
 
@@ -168,9 +217,7 @@ describe( 'v6.Vector2D', function ()
     {
       it( 'works', function ()
       {
-        new Vector2D( 4, 2 ).magSq()
-          .should
-            .equal( ( 4 * 4 ) + ( 2 * 2 ) );
+        new Vector2D( 4, 2 ).magSq().should.equal( ( 4 * 4 ) + ( 2 * 2 ) );
       } );
     } );
 
@@ -178,9 +225,7 @@ describe( 'v6.Vector2D', function ()
     {
       it( 'works', function ()
       {
-        new Vector2D( 4, 2 ).mag()
-          .should
-            .equal( Math.sqrt( ( 4 * 4 ) + ( 2 * 2 ) ) );
+        new Vector2D( 4, 2 ).mag().should.equal( Math.sqrt( ( 4 * 4 ) + ( 2 * 2 ) ) );
       } );
     } );
 
@@ -189,10 +234,9 @@ describe( 'v6.Vector2D', function ()
       it( 'works', function ()
       {
         var vector = new Vector2D( 4, 2 );
-
-        vector.clone()
-          .should
-            .deep.equal( vector );
+        var clone = vector.clone();
+        clone.should.deep.equal( vector );
+        clone.should.not.equal( vector );
       } );
     } );
 
@@ -200,9 +244,20 @@ describe( 'v6.Vector2D', function ()
     {
       it( 'works', function ()
       {
-        new Vector2D( 4.321, 2.345 ).toString()
-          .should
-            .equal( 'v6.Vector2D { x: 4.32, y: 2.35 }' );
+        new Vector2D( 4.321, 2.345 ).toString().should.equal( 'v6.Vector2D { x: 4.32, y: 2.35 }' );
+      } );
+    } );
+
+    describe( 'new v6.Vector2D.dist', function ()
+    {
+      it( 'works #1', function ()
+      {
+        new Vector2D( 1, 2 ).dist( new Vector2D( 8, 4 ) ).should.equal( Math.sqrt( ( 7 * 7 ) + ( 2 * 2 ) ) );
+      } );
+
+      it( 'works #2', function ()
+      {
+        new Vector2D( 1, 2 ).dist( { x: 8, y: 4 } ).should.equal( Math.sqrt( ( 7 * 7 ) + ( 2 * 2 ) ) );
       } );
     } );
   } );
@@ -211,9 +266,7 @@ describe( 'v6.Vector2D', function ()
   {
     it( 'works', function ()
     {
-      Vector2D.random().mag()
-        .should
-          .closeTo( 1, 1e-8 );
+      Vector2D.random().mag().should.closeTo( 1, 1e-8 );
     } );
   } );
 
@@ -222,10 +275,7 @@ describe( 'v6.Vector2D', function ()
     it( 'works', function ()
     {
       var angle = Math.PI / 180 * 45;
-
-      Vector2D.fromAngle( angle )
-        .should
-          .like( { x: Math.cos( angle ), y: Math.sin( angle ) } );
+      Vector2D.fromAngle( angle ).should.like( { x: Math.cos( angle ), y: Math.sin( angle ) } );
     } );
   } );
 } );
